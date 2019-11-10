d2d4176286079577e15b60b14382851c89545c7b
Maggie Chen <missmag@live.cn>
2019-11-10 17:01:22 -0500

    use babel to not write constructor

--- src/index.js
+++ src/index.js
@@ -2,12 +2,6 @@ import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 
 class App extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = { lat: null, errorMessage: '' }
-  }
-
   state = { lat: null, errorMessage: '' }
 
   componentDidMount() {
